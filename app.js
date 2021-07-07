fc4b2403547d8c71c907a2d270f72fedc961a5ed
Sakina <sakina.nadiadi@gmail.com>
2021-07-07 00:03:07 -0400

    created the display modal for read more under Our Programs

--- app.js
+++ app.js
@@ -8,12 +8,31 @@ function mobileMenu() {
   navMenu.classList.toggle("active");
 }
 
-// // Closing the hamburger menu
-// const navLink = document.querySelectorAll("nav li");
+// Modal display
 
-// navLink.forEach((n) => n.addEventListener("click", closeMenu));
+// Get the modal
+const modal = document.querySelector("#myModal");
 
-// function closeMenu() {
-//   hamburger.classList.remove("active");
-//   navMenu.classList.remove("active");
-// }
+// Get the button that opens the modal
+const btn = document.querySelector(".programContent .btn");
+
+// When the user clicks on the button, open the modal
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+  modal.style.display = "block";
+});
+
+// Get the <span> element that closes the modal
+const span = document.querySelector(".close");
+
+// When the user clicks on <span> (x), close the modal
+span.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+
+// When the user clicks anywhere outside of the modal, close it
+window.addEventListener("click", function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
